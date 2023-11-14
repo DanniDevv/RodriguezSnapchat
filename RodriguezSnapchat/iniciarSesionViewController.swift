ce096d81f2ea47c79dd70039a198fc74315fd4ae
Dannidev <dante.rodriguez.c@tecsup.edu.pe>
2023-11-14 17:57:51 -0500

    actualizacion

--- RodriguezSnapchat/iniciarSesionViewController.swift
+++ RodriguezSnapchat/iniciarSesionViewController.swift
@@ -7,25 +7,34 @@
 
 import UIKit
 import FirebaseAuth
-
+import GoogleSignIn
 
 class iniciarSesionViewController: UIViewController {
 
     @IBOutlet weak var emailTextField: UITextField!
-    
     @IBOutlet weak var passwordTextField: UITextField!
-    
+    @IBOutlet weak var googleSignInButton: UIButton!
     
     @IBAction func iniciarSesionTapped(_ sender: Any) {
         Auth.auth().signIn(withEmail:emailTextField.text!, password: passwordTextField.text!){(user,error) in
             print("Intentando Iniciar Sesion")
             if error != nil{
-                print("Se presento el siguiente: \(error)")
+                print("Se presento el siguiente error: \(error)")
+                Auth.auth().createUser(withEmail: self.emailTextField.text!, password:  self.passwordTextField.text!, completion: {(user, error) in print("Intentando crear un usuario")
+                    if error != nil{
+                        print("Se presento el siguiente error al crear el: \(error)")
+                    }else{
+                        print("El usuaio fue creado exitosamente")
+                        self.performSegue(withIdentifier: "iniciarsesionsegue", sender: nil)
+                    }
+                })
             }else{
-                print("Inicio de sesion exitoso")
+                print("Inicio de Sesion Exitoso")
+                self.performSegue(withIdentifier: "iniciarsesionsegue", sender: nil)
             }
         }
     }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
